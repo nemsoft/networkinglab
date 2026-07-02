8e0d30da786baa8721f087a964a82d0a958999d0
nobody-somobody <pewdsupbot1@gmail.com>
2026-07-02 11:53:10 +0200

    formattering og reply funktion

--- src/ChatServer3.java
+++ src/ChatServer3.java
@@ -9,11 +9,11 @@ import java.util.Scanner;
 
 public class ChatServer3{
     static ArrayList<ClientHandler3> clients;
-    public void announce(ClientHandler3 client, String message){
+    public void announce(ClientHandler3 client, String message) {
         System.out.println(message);
-        for(ClientHandler3 c:clients){
+        for(ClientHandler3 c:clients) {
             if(c.name==null) break;
-            if(c!=client&&c.name!=null){
+            if(c!=client&&c.name!=null) {
                 try {
                     PrintWriter output = new PrintWriter(c.socket.getOutputStream(), true);
                     output.println(message);
@@ -21,15 +21,26 @@ public class ChatServer3{
             }
         }
     }
-    public boolean message(String sender, String receiver, String message){
-        for(ClientHandler3 c:clients){
+    public boolean reply(ClientHandler3 sender, String message) {
+        if(sender.replier!=null) {
+            try {
+                PrintWriter output = new PrintWriter(sender.replier.socket.getOutputStream(), true);
+                output.println(sender.name+" -> "+sender.replier.name+": "+message);
+                System.out.println(sender.name+" -> "+sender.replier.name+": "+message);
+            } catch (IOException e) {}
+        }
+        return true;
+    }
+    public boolean message(ClientHandler3 sender, String receiver, String message) {
+        for(ClientHandler3 c:clients) {
             if(c.name==null) break;
-            if(c.name.equals(receiver)){
+            if(c.name.equals(receiver)) {
                 try {
                     PrintWriter output = new PrintWriter(c.socket.getOutputStream(), true);
-                    String messageComp = sender+" -> "+receiver+": "+message;
-                    output.println(messageComp);
-                    System.out.println(messageComp);
+                    output.println(sender.name+" -> "+receiver+": "+message);
+                    System.out.println(sender.name+" -> "+receiver+": "+message);
+                    sender.replier = c;
+                    c.replier = sender;
                     return true;
                 } catch (IOException e) {
                     System.out.println("Kan ikke sende besked til "+c.name);
@@ -38,29 +49,28 @@ public class ChatServer3{
         }
         return false;
     }
-    public String list(){
+    public String list() {
         String list = "";
         int unnamed = 0;
-        for(ClientHandler3 c:clients){
-            if(c.name==null){
+        for(ClientHandler3 c:clients) {
+            if(c.name==null) {
                 unnamed+=1;
             } else {
                 list = list.concat("- "+c.name+"\r\n");
             }
         }
-        if(unnamed>0){
-            list = list.concat("unnamed x"+unnamed);
+        if(unnamed>0) {
+            list = list.concat("unnamed x"+unnamed+"\r\n");
         }
-        return list.substring(0, list.length());
+        return list.substring(0, list.length()-1);
     }
-    public boolean nameTaken(String name){
-        for(ClientHandler3 c:clients){
-            if(c.name==null) break;
-            if(c.name.equals(name)) return true;
+    public boolean nameTaken(String name) {
+        for(ClientHandler3 c:clients) {
+            if(c.name!=null&&c.name.equals(name)) return true;
         }
         return false;
     }
-    public void remove(ClientHandler3 client){
+    public void remove(ClientHandler3 client) {
         clients.remove(client);
     }
     public static void main(String[] args) throws IOException{
@@ -70,18 +80,18 @@ public class ChatServer3{
         clients = new ArrayList<>();
         Thread commands = new Thread(() -> {
             Scanner scanner = new Scanner(System.in);
-            while(scanner.hasNext()){
+            while(scanner.hasNext()) {
                 String input = scanner.nextLine();
-                for(ClientHandler3 c:clients){
-                    if(input.startsWith("list")){
-                        if(c.name==null){
+                for(ClientHandler3 c:clients) {
+                    if(input.startsWith("list")) {
+                        if(c.name==null) {
                             System.out.println(c.addr+" unnamed");
                         } else {
                             System.out.println(c.addr+" - "+c.name);
                         }
                     }
-                    if(input.startsWith("kick")){
-                        if(c.name.equals(input.substring(5))||c.addr.equals(input.substring(5))){
+                    if(input.startsWith("kick")) {
+                        if(c.name.equals(input.substring(5))||c.addr.equals(input.substring(5))) {
                             try {
                                 c.socket.close();
                                 clients.remove(c);
--- src/ClientHandler3.java
+++ src/ClientHandler3.java
@@ -3,6 +3,7 @@ import java.net.*;
 
 public class ClientHandler3 implements Runnable{
     final private ChatServer3 server;
+    ClientHandler3 replier;
     Socket socket;
     String addr;
     String name = null;
@@ -21,11 +22,11 @@ public class ClientHandler3 implements Runnable{
             );
             output.println("Welcome "+addr);
             String newName = " ";
-            while(newName==null||newName.contains(" ")||newName.isBlank()){
+            while(newName==null||newName.contains(" ")||newName.isBlank()) {
                 if(newName!=null) output.printf("Enter username without spaces: ");
                 newName = input.readLine();
                 if(newName.startsWith("�")) newName = newName.substring(21);
-                if(server.nameTaken(newName)){
+                if(server.nameTaken(newName)) {
                     newName = null;
                     output.printf("Username taken, try again: ");
                 }
@@ -34,18 +35,19 @@ public class ClientHandler3 implements Runnable{
             output.println("Welcome "+name);
             server.announce(this, name+" joined");
             String message;
-            while ((message = input.readLine()) != null){
-                String[] messageComp = message.split("\\s+", 3);
-                if(message.equals("list")){
+            while ((message = input.readLine()) != null) {
+                String[] messageComp = message.split("\\s+");
+                if(message.equals("list")) {
                     output.println(server.list());
-                } else if(messageComp[0].equals("msg")){
-                    server.message(name, messageComp[1], messageComp[2]);
+                } else if(messageComp[0].equals("msg")) {
+                    server.message(this, messageComp[1], message.split("\\s+",3)[2]);
+                } else if (messageComp[0].equals("reply")) {
+                    server.reply(this, message.split("\\s+",2)[1]);
                 } else {
-                    server.announce(this, name+": " + message);
+                    server.announce(this, name+": "+message);
                 }
             }
-        } catch (IOException e){}
-        System.out.println(name+" disconnected");
+        } catch (IOException e) {}
         server.announce(this, name+" disconnected");
         server.remove(this);
     }
